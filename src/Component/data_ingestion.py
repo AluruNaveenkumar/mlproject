0dbfd3473e65fc33722c4f804831cf74b5887f30
AluruNaveenkumar <agnavin1626@gmail.com>
2024-04-16 07:57:01 +0530

    Data Transformation

--- src/Component/data_ingestion.py
+++ src/Component/data_ingestion.py
@@ -7,6 +7,9 @@ from src.exception import CustomException
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.Component.data_transformation import DataTransformation
+from src.Component.data_transformation import DataTransformationConfig
+
 
 @dataclass
 class DataIngestionConfig:
@@ -37,9 +40,19 @@ class DataIngestion:
 
             logging.info('Ingestion of data is completed')
 
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+
         except Exception as e:
             raise CustomException(e,sys)
         
 if __name__=='__main__':
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    try: 
+        train_data,test_data=obj.initiate_data_ingestion()
+    except Exception as e:
+        raise CustomException(e,sys)
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
